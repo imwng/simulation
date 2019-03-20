dc09a525098688f239897873cc2a2baf85d7992a
imwng <48729383+imwng@users.noreply.github.com>
2019-03-21 02:06:01 +0800

    Update Validate.js

--- Validate/Validate.js
+++ Validate/Validate.js
@@ -1,7 +1,24 @@
 // 验证类 ES6实现
 class Validate {
-  constructor (rules = [], message = []) {
+  constructor (rules = [], message = [], field = []) {
     // @rules: 数据的验证规则
-    // @message: 数据验证失败的提示信息
+    // @message: 验证提示信息
+    // @field: 验证字段描述信息
+    
+  }
+  
+  /**
+   * 数据自动验证
+   * @param {Array} data 数据
+   * @param {Array} rules 验证规则
+   * @param {String} scene 验证场景
+   * @return {Boolean}
+   */
+  function check (data, rules = [], scene = '') {
+    this.error = [];
+    
+    if (rules.length === 0) {
+      rules = this.rule;
+    }
   }
 }
